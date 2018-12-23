804deb3ed61e3995dabd47a54f0973f71329ef2f
Edward <enilsson9@hotmail.com>
2018-12-22 18:59:42 -0600

    Modified layout and CRUD views

--- view/anax/v2/layout/default.php
+++ view/anax/v2/layout/default.php
@@ -26,8 +26,12 @@ $title = ($title ?? "No title") . ($baseTitle ?? " | No base title defined");
 
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://use.fontawesome.com/e5579368c4.js"></script>
-    <link rel="stylesheet" href="css/style.css">
+
+    <?php if (isset($stylesheets)) : ?>
+        <?php foreach ($stylesheets as $stylesheet) : ?>
+            <link rel="stylesheet" type="text/css" href="<?= asset($stylesheet) ?>">
+        <?php endforeach; ?>
+    <?php endif; ?>
 </head>
 <body>
 
@@ -64,6 +68,7 @@ $title = ($title ?? "No title") . ($baseTitle ?? " | No base title defined");
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://use.fontawesome.com/e5579368c4.js"></script>
 <?php if (isset($stylesheets)) : ?>
     <?php foreach ($javascripts as $javascript) : ?>
     <script async src="<?= asset($javascript) ?>"></script>

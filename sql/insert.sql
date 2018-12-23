733c3ae35d15985362ebb0a3e0c802c553ee43f9
Edward <enilsson9@hotmail.com>
2018-12-22 18:58:51 -0600

    Changed image path to links

--- sql/insert.sql
+++ sql/insert.sql
@@ -5,9 +5,9 @@
 
 DELETE FROM `Book`;
 INSERT INTO `Book` (`image`, `title`, `author`) VALUES
-    ("img/books/1.jpeg", "In search of lost time", "Marcel Proust"),
-    ("img/books/2.jpg", "Don Quixote", "our-vision"),
-    ("img/books/3.jpg", "Ulysses", "James Joyce"),
-    ("img/books/4.jpg", "The Great Gatsby", "F. Scott Fitzgerald"),
-    ("img/books/5.jpg", "Moby Dick", "Herman Melville")
+    ("http://www.student.bth.se/~edni17/dbwebb-kurser/ramverk1/me/redovisa/htdocs/img/books/1.jpeg", "In search of lost time", "Marcel Proust"),
+    ("http://www.student.bth.se/~edni17/dbwebb-kurser/ramverk1/me/redovisa/htdocs/img/books/2.jpg", "Don Quixote", "our-vision"),
+    ("http://www.student.bth.se/~edni17/dbwebb-kurser/ramverk1/me/redovisa/htdocs/img/books/3.jpg", "Ulysses", "James Joyce"),
+    ("http://www.student.bth.se/~edni17/dbwebb-kurser/ramverk1/me/redovisa/htdocs/img/books/4.jpg", "The Great Gatsby", "F. Scott Fitzgerald"),
+    ("http://www.student.bth.se/~edni17/dbwebb-kurser/ramverk1/me/redovisa/htdocs/img/books/5.jpg", "Moby Dick", "Herman Melville")
 ;
